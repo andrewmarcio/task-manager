87a1d282b35547dd6bcf8cd30d5e26afe3d1ce34
andrewmarcio <andrewmarcio21@gmail.com>
2025-01-26 22:44:22 -0300

    feat: add home page list

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -2,15 +2,42 @@ import { AppBar } from "@presentation/components/app-bar"
 import { Button } from "@presentation/components/button"
 import { Input } from "@presentation/components/form/input"
 import { Select } from "@presentation/components/form/select"
-import { Table, TableActions, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@presentation/components/table"
-import { usePagination } from "@presentation/hooks/use-pagination"
+import { 
+    Table, 
+    TableActions, 
+    TableBody, 
+    TableCell, 
+    TableHead, 
+    TableHeader, 
+    TableRow 
+} from "@presentation/components/table"
+import { 
+    ChevronLeft, 
+    ChevronRight, 
+    ChevronsLeft, 
+    ChevronsRight, 
+    List, 
+    LoaderCircleIcon 
+} from "lucide-react"
 import { memo } from "react"
-import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "lucide-react"
 import { useHomePage } from "./use-home-page"
+import { TaskStatus as TaskStatusEnum } from "@domain/task/task-status.enum"
+import { TaskStatus } from "@presentation/components/task-status"
 
 const Home = memo(() => {
-    const { actionOptions } = useHomePage()
-    const { currentPage, totalPages, rowsPerPage, rowsPerPageOptions, handleChangePerPage } = usePagination()
+    const {
+        loading,
+        tasks,
+        currentPage,
+        totalPages,
+        rowsPerPage,
+        rowsPerPageOptions,
+        register,
+        actionOptions,
+        handleChangePerPage,
+        handleChangeStatus,
+        handlePageChange
+    } = useHomePage()
 
     return <div className="flex flex-col w-full h-full justify-center">
         <div className="flex items-center justify-center">
@@ -28,7 +55,21 @@ const Home = memo(() => {
             <div className="flex flex-col items-center pt-5 pb-6">
                 <div className="w-[85dvw] flex flex-col gap-6 rounded-lg p-4 overflow-hidden bg-white drop-shadow-2xl">
                     <div className="flex flex-col gap-4">
-                        <Input className="w-100 md:w-80" placeholder="filter tasks..." />
+                        <div className="flex flex-col gap-2 md:flex-row md:gap-4">
+                            <Input {...register("searchText")} className="w-100 md:w-80" placeholder="filter tasks..." />
+                            <Select
+                                className="w-100 md:w-36"
+                                placeholder="Status"
+                                onValueChange={handleChangeStatus}
+                                defaultValue="all"
+                                options={[
+                                    { label: "All", value: "all" },
+                                    { label: "Pending", value: TaskStatusEnum.Pending },
+                                    { label: "In Progress", value: TaskStatusEnum.InProgress },
+                                    { label: "Complete", value: TaskStatusEnum.Complete },
+                                ]}
+                            />
+                        </div>
                         <div className="border rounded-lg">
                             <Table>
                                 <TableHeader>
@@ -41,19 +82,28 @@ const Home = memo(() => {
                                     </TableRow>
                                 </TableHeader>
                                 <TableBody>
-                                    {Array.from({ length: rowsPerPage }).flatMap((_, index) =>
+                                    {!loading && tasks.flatMap((task, index) =>
                                         <TableRow key={index}>
-                                            <TableCell className="w-fit md:w-72">549a2699-1a6f-43a5-92a7-17dccd180cc5</TableCell>
-                                            <TableCell className="truncate max-w-36">Task {index + 1}</TableCell>
-                                            <TableCell className="truncate max-w-36">Teste description</TableCell>
-                                            <TableCell>Pending</TableCell>
+                                            <TableCell className="w-fit md:w-72">{task.identifier}</TableCell>
+                                            <TableCell className="truncate max-w-36">{task.title}</TableCell>
+                                            <TableCell className="truncate max-w-36">{task.description}</TableCell>
+                                            <TableCell><TaskStatus type={task.status} /></TableCell>
                                             <TableCell>
-                                                <TableActions actions={actionOptions("549a2699-1a6f-43a5-92a7-17dccd180cc5")} />
+                                                <TableActions actions={actionOptions(task.identifier)} />
                                             </TableCell>
                                         </TableRow>
                                     )}
                                 </TableBody>
                             </Table>
+                            {!loading && !tasks.length && (
+                                <div className="w-full h-32 flex flex-row gap-4 items-center justify-center">
+                                    <List className="h-8 w-8 text-gray-600" />
+                                    <p className="text-sm">No tasks found</p>
+                                </div>
+                            )}
+                            {loading && <div className="w-full h-32 flex items-center justify-center">
+                                <LoaderCircleIcon className="animate-spin h-12 w-12 text-gray-600" />
+                            </div>}
                         </div>
                         <div className="flex flex-col md:flex-row w-100 gap-4 md:gap-6 items-center justify-end text-sm font-medium">
                             <div className="flex flex-row gap-6 items-center justify-between">
@@ -71,16 +121,16 @@ const Home = memo(() => {
                                 </div>
                             </div>
                             <div className="flex flex-row gap-1 items-center justify-center">
-                                <Button className="w-8 h-8" variant={"outline"}>
+                                <Button className="w-8 h-8" variant={"outline"} onClick={handlePageChange("toFirstPage")}>
                                     <ChevronsLeft className="w-4 h-4" />
                                 </Button>
-                                <Button className="w-8 h-8" variant={"outline"}>
+                                <Button className="w-8 h-8" variant={"outline"} onClick={handlePageChange("prev")}>
                                     <ChevronLeft className="w-4 h-4" />
                                 </Button>
-                                <Button className="w-8 h-8" variant={"outline"}>
+                                <Button className="w-8 h-8" variant={"outline"} onClick={handlePageChange("next")}>
                                     <ChevronRight className="w-4 h-4" />
                                 </Button>
-                                <Button className="w-8 h-8" variant={"outline"}>
+                                <Button className="w-8 h-8" variant={"outline"} onClick={handlePageChange("toLastPage")}>
                                     <ChevronsRight className="w-4 h-4" />
                                 </Button>
                             </div>
--- src/presentation/pages/home/use-home-page.tsx
+++ src/presentation/pages/home/use-home-page.tsx
@@ -1,15 +1,137 @@
+import { TaskStatus } from "@domain/task/task-status.enum"
+import { ListTaskDTO } from "@domain/task/task.dto"
+import { Task } from "@domain/task/task.entity"
+import { httpClient } from "@infra/http-client"
 import { TableAction } from "@presentation/components/table/table"
+import { useDebounce } from "@presentation/hooks/use-debounce"
+import { usePagination } from "@presentation/hooks/use-pagination"
+import { useToast } from "@presentation/hooks/use-toast"
+import { taskService } from "@services/task.service"
 import { Pencil, Trash2 } from "lucide-react"
-import { useCallback } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { useForm, useWatch } from "react-hook-form"
 
 export function useHomePage() {
 
+    const [tasks, setTasks] = useState<Task[]>([])
+
+    const [loading, setLaoding] = useState<boolean>(false)
+
+    const { toast } = useToast()
+
+    const { control, register, getValues, setValue } = useForm({
+        defaultValues: {
+            searchText: "",
+            status: undefined
+        }
+    })
+
+    const {
+        currentPage,
+        totalPages,
+        rowsPerPage,
+        rowsPerPageOptions,
+        changePerPage,
+        prev,
+        next,
+        toFirstPage,
+        toLastPage,
+    } = usePagination()
+
+    const searchText = useWatch({
+        control,
+        name: "searchText"
+    })
+
+    const searchTitle = useDebounce(searchText)
+
+    const getTasks = useCallback(async (params: ListTaskDTO) => {
+        const service = taskService({ client: httpClient })
+        return await service.list(params)
+    }, [])
+
+    const showRequestErrorMessage = useCallback(() => {
+        toast({
+            variant: "destructive",
+            title: "Error",
+            description: "It was not possible to search"
+        })
+    }, [])
+
+    const handleChangePerPage = useCallback(async (value: string) => {
+        setLaoding(true)
+        changePerPage(value)
+        try {
+            const params = getValues()
+            const { data } = await getTasks({ ...params, currentPage, perPage: Number(value) })
+            setTasks(data)
+        } catch (error) {
+            showRequestErrorMessage()
+        }
+
+        setLaoding(false)
+    }, [currentPage])
+
+    const handleChangeStatus = useCallback(async (status: TaskStatus & "all") => {
+        setLaoding(true)
+        setValue("status", status as any)
+        const selectedStatus = status !== "all" ? status : undefined
+        try {
+            const params = getValues()
+            const { data } = await getTasks({ ...params, status: selectedStatus, currentPage, perPage: rowsPerPage })
+            setTasks(data)
+        } catch (error) {
+            showRequestErrorMessage()
+        }
+        setLaoding(false)
+    }, [rowsPerPage, currentPage])
+
+    const searchByTitle = useCallback(async () => {
+        setLaoding(true)
+        try {
+            const params = getValues()
+            const { data } = await getTasks({ ...params, currentPage, perPage: rowsPerPage })
+            setTasks(data)
+        } catch (error) {
+            showRequestErrorMessage()
+        }
+        setLaoding(false)
+    }, [rowsPerPage, currentPage])
+
+    const handlePageChange = useCallback(
+        (type: "toFirstPage" | "toLastPage" | "prev" | "next") => {
+            const request = async (page: number) => {
+                setLaoding(true)
+                try {
+                    const params = getValues()
+                    const { data } = await getTasks({ ...params, currentPage: page, perPage: rowsPerPage })
+                    setTasks(data)
+                } catch (error) {
+                    showRequestErrorMessage()
+                }
+                setLaoding(false)
+            }
+
+            return {
+                toFirstPage() { toFirstPage(request) },
+                toLastPage() { toLastPage(request) },
+                prev() { prev(request) },
+                next() { next(request) },
+            }[type]
+        }, [
+            rowsPerPage,
+            prev,
+            next,
+            toFirstPage,
+            toLastPage,
+        ])
+
     const handleEdit = useCallback((identifier: string) => () => {
         alert("Edit")
-     }, [])
+    }, [])
     const handleRemove = useCallback((identifier: string) => () => {
         alert("Remove")
-     }, [])
+    }, [])
 
     const actionOptions = useCallback((identifier: string) => {
         return [
@@ -26,7 +148,28 @@ export function useHomePage() {
         ] satisfies TableAction[]
     }, [])
 
+    useEffect(() => {
+        if (searchTitle) {
+            searchByTitle()
+        }
+    }, [searchTitle])
+
+    useEffect(() => {
+        searchByTitle()
+    }, [])
+
+
     return {
-        actionOptions
+        loading,
+        tasks,
+        currentPage,
+        totalPages,
+        rowsPerPage,
+        rowsPerPageOptions,
+        register,
+        actionOptions,
+        handleChangePerPage,
+        handleChangeStatus,
+        handlePageChange
     }
 }
